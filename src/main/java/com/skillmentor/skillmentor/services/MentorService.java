6a176907b405b5bb3582ae9d8c468361a59b475e
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-26 22:19:12 +0530

    Update Mentor

---
+++ src/main/java/com/skillmentor/skillmentor/services/MentorService.java
@@ -0,0 +1,39 @@
+package com.skillmentor.skillmentor.services;
+
+import com.skillmentor.skillmentor.entities.Mentor;
+import com.skillmentor.skillmentor.repositories.MentorRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MentorService {
+
+    private final MentorRepository mentorRepository;
+    private final ModelMapper modelMapper;
+
+    public Mentor createNewMentor(Mentor mentor) {
+        return mentorRepository.save(mentor);
+    }
+
+    public List<Mentor> getAllMentors() {
+        return mentorRepository.findAll();
+    }
+
+    public Mentor getMentorById(Long id) {
+        return mentorRepository.findById(id).get();
+    }
+
+    public Mentor updateMentorById(Long id, Mentor updatedMentor) {
+        Mentor mentor = mentorRepository.findById(id).get();
+        modelMapper.map(updatedMentor, mentor);
+        return mentorRepository.save(mentor);
+    }
+
+    public void deleteMentorById(Long id) {
+        mentorRepository.deleteById(id);
+    }
+}
