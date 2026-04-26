1e3f4a2882c58b6ae4dfadf50e40d41418fa7a44
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-26 22:19:19 +0530

    Update Subject

--- src/main/java/com/skillmentor/skillmentor/controllers/SubjectController.java
+++ src/main/java/com/skillmentor/skillmentor/controllers/SubjectController.java
@@ -3,12 +3,11 @@ package com.skillmentor.skillmentor.controllers;
 import com.skillmentor.skillmentor.dto.SubjectDTO;
 import com.skillmentor.skillmentor.entities.Subject;
 import com.skillmentor.skillmentor.services.SubjectService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,20 +20,19 @@ public class SubjectController {
 
 
     @GetMapping
-    public List<Subject> getAllSubjects(@RequestParam(name = "name", defaultValue = "all") String name) {
+    public List<Subject> getAllSubjects() {
 //        String result = subjects.toString();
 //        System.out.println(result);
         return subjectService.getAllSubjects();
     }
 
-//    @GetMapping("{id}")
-//    public Subject getSubjectById(@PathVariable int id) {
-//        System.out.println("Getting subject by id " + id);
-//        return subjectService.get(id);
-//    }
+    @GetMapping("{id}")
+    public Subject getSubjectById(@PathVariable Long id) {
+        return subjectService.getSubjectById(id);
+    }
 
     @PostMapping
-    public Subject createSubject(@Validated @RequestBody SubjectDTO subjectDTO) {
+    public Subject createSubject(@Valid @RequestBody SubjectDTO subjectDTO) {
 
         // Validation to check subject name length
 //        if (subject.getSubjectName().length() > 20) {
@@ -58,16 +56,14 @@ public class SubjectController {
         return subjectService.addNewSubject(subject);
     }
 
-//    @PutMapping("{id}")
-//    public String updateSubject(@RequestBody String updatedSubject) {
-//        System.out.println("PUT" + updatedSubject);
-//        return "Updated subject";
-//    }
-//
-//    @DeleteMapping("{id}")
-//    public String deleteSubject(@PathVariable int id) {
-//        System.out.println("DELETE");
-//        subjects.remove(id);
-//        return subjects.toString();
-//    }
+    @PutMapping("{id}")
+    public Subject updateSubject(@PathVariable Long id, @RequestBody SubjectDTO updatedSubjectDTO) {
+        Subject subject = modelMapper.map(updatedSubjectDTO, Subject.class);
+        return subjectService.updateSubjectById(id, subject);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteSubject(@PathVariable Long id) {
+        subjectService.deleteSubject(id);
+    }
 }
--- src/main/java/com/skillmentor/skillmentor/entities/Subject.java
+++ src/main/java/com/skillmentor/skillmentor/entities/Subject.java
@@ -1,23 +1,37 @@
 package com.skillmentor.skillmentor.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "subject")
 public class Subject {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, name = "subject_name", length = 20)
     private String subjectName;
+
+    @Column(nullable = false)
     private String description;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
 
     // Get all getters in one annotation using lombok @Getter
     // Set all setters in one annotation using lombok @Setter
